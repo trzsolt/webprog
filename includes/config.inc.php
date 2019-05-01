e3de3a8d0ee89aece3158408cc83c863e2bbf80f
trzsolt <50169256+trzsolt@users.noreply.github.com>
2019-05-01 19:39:00 +0200

    Initial commit

---
+++ includes/config.inc.php
@@ -0,0 +1,30 @@
+﻿<?php
+$ablakcim = array(
+    'cim' => 'Nebuló Alapítvány',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'Nebuló Alapítvány',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Nebuló Alapítvány'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menun' => array(1,1)),
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
---
+++ templates/index.tpl.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+<?php if(file_exists('./logicals/'.$keres['fajl'].'.php')) { include("./logicals/{$keres['fajl']}.php"); } ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
+	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
+	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
+</head>
+<body>
+	<div id="fejlec">
+			<div id="logo">
+				<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
+			</div>
+		    <div id="kereso">					
+                <script>
+                (function() {
+                    var cx = '012004761518964643184:vz6p7dmwwgk';
+                    var gcse = document.createElement('script');
+                    gcse.type = 'text/javascript';
+                    gcse.async = true;
+                    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(gcse, s);
+                  })();
+                </script>
+                <gcse:search></gcse:search>
+            </div>
+		<?php if (isset($fejlec['motto'])) { ?><h2><?= $fejlec['motto'] ?></h2><?php } ?>
+		<?php if(isset($_SESSION['login'])) { ?>Bejelentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
+	</div>
+
+    <div id="wrapper">
+        <aside id="nav">
+            <nav>
+                <ul>
+					<?php foreach ($oldalak as $url => $oldal) { ?>
+						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+							<li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
+							<a href="<?= ($url == '/') ? '.' : ('?oldal=' . $url) ?>">
+							<?= $oldal['szoveg'] ?></a>
+							</li>
+						<?php } ?>
+					<?php } ?>
+                </ul>
+            </nav>
+        </aside>
+        <div id="content">
+            <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
+        </div>
+    </div>
+    <footer>
+        <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
+		&nbsp;
+        <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
+    </footer>
+</body>
+</html>
